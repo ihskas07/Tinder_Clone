9df029d93d23e8d3c694d87748b0ea23b6d77252
Your Name <you@example.com>
2022-08-02 01:05:00 +0530

    Final commit

--- src/Chats.js
+++ src/Chats.js
@@ -9,7 +9,7 @@ function Chats() {
     <div className="chats">
         <Chat                    //components in chat page
         name="Sam"
-        message="Hey you!!"
+        message="Hey you!"
         timestamp="40 seconds ago"
         profilePic="https://i.pinimg.com/474x/12/3a/a3/123aa37e604bc4fdc959dd7d9dba2a75.jpg"
       />
@@ -21,13 +21,13 @@ function Chats() {
       />
       <Chat
         name="Jack"
-        message="Hey,see you in 5!"
+        message="Hey,see you in 5"
         timestamp="2 minutes ago"
         profilePic="https://i.pinimg.com/736x/c3/d5/3f/c3d53f0489b026890d2ecc1483522b57.jpg"
       />
        <Chat
         name="Mark"
-        message="Wanna get some Coffee?!"
+        message="Wanna get some Coffee?"
         timestamp="3 minutes ago"
         profilePic="https://i.pinimg.com/736x/09/d1/df/09d1dfd3ad53cb18677ea097bd5e5bdb.jpg"
       />
